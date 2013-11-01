a67a1f558c4cae37b21afa2e6000e86b2d24dc42
Aleksey Syromyatnikov <alekssyrom@gmail.com>
2013-11-01 22:34:10 +0400

    more changes

--- Parser.cpp
+++ Parser.cpp
@@ -67,8 +67,16 @@ void Parser::_printMistakes() {
 void Parser::_printHelp() {
   std::cout << "FEATURED OPTIONS\n\t--help\n\t\tprint help\n\t";
   std::cout << "--time\n\t\t";
-  std::cout << "set data and time on which system data and time will be updated. Format:";
-  std::cout << "Month/Day hour:minute, you can use 'every' or 'every{'interval'}' instead of each instance\n";
+  std::cout << "set data and time for the updating data and time on the machine\n\t\tFormat:\n\t\t\t";
+  std::cout << "Month/Day hour:minute, where:\n\t\t\t\t";
+  std::cout << "Month: between 1 and 12 or 'every' for every month or 'every{'interval'}',\n\t\t\t\t\t";
+  std::cout << "where interval is between 1 and 12\n\t\t\t\t";
+  std::cout << "Day: between 1 and 31 or 'every' for every day or 'every{'interval'}',\n\t\t\t\t\t";
+  std::cout << "where interval is between 1 and 31\n\t\t\t\t";
+  std::cout << "hour: between 0 and 23 or 'every' for every hour or 'every{'interval'}',\n\t\t\t\t\t";
+  std::cout << "where interval is between 1 and 23\n\t\t\t\t";
+  std::cout << "minute: between 0 and 59 or 'every' for every minute or 'every{'interval'}',\n\t\t\t\t\t";
+  std::cout << "where interval is between 1 and 59\n\t\t\t\t\n";
 }
 
 void Parser::_runCommands() {
@@ -194,7 +202,7 @@ void Parser::_setTimeInCrontabFile(int pos) {
     flag = true;
   }
   if(day == "every") 
-    dayI.mode = -1;
+    dayI.mode = 0;
   if(flagDayEvery)
     dayI.mode = 2;
   ModeAndValue hourI = {1,0};
@@ -211,7 +219,7 @@ void Parser::_setTimeInCrontabFile(int pos) {
     flag = true;
   }
   if(hour == "every") 
-    hourI.mode = -1;
+    hourI.mode = 0;
   if(flagHourEvery && hourI.value == 0 && hour == "0") {
     _addMistake("--time: interval of hours shouldn't be 0");
     flag = true;
@@ -232,7 +240,7 @@ void Parser::_setTimeInCrontabFile(int pos) {
     flag = true;
   }
   if(minute == "every") 
-    minuteI.mode = -1;
+    minuteI.mode = 0;
   if(flagMinuteEvery && minuteI.value == 0 && minute == "0") {
     _addMistake("--time: interval of minutes shouldn't be 0");
     flag = true;
