2f64f9cbb8d2a564cb09af9f6c2c9fea8b7c48c0
Aleksey Syromyatnikov <alekssyrom@gmail.com>
2013-11-07 20:36:18 +0400

    deletes some files

--- Arguments.cpp
+++
@@ -1,9 +0,0 @@
-#include "Arguments.h"
-
-Arguments& Arguments::operator=(const Arguments& given) {
-  this->minute = given.minute;
-  this->hour = given.hour;
-  this->day = given.day;
-  this->month = given.month;
-  return *this;
-}
