4618be240eeb07b7e65014d0a98dc6e2cdf8bf3b
Aleksey Syromyatnikov <alekssyrom@gmail.com>
2013-11-16 13:11:57 +0400

    Changes in printHelp

--- src/OutputHandler.cpp
+++ src/OutputHandler.cpp
@@ -2,8 +2,11 @@
 
 void OutputHandler::printHelp() {
   std::cout << "FEATURED OPTIONS\n\t--help\n\t\tprint help\n\t";
-  std::cout << "--server\n\t\tset an ntp server for time updating\n\t";
-  std::cout << "--time\n\t\t";
+  std::cout << "--server\n\t\tset an ntp server for time updating\n\t\t";
+  std::cout << "When it nessesary to set more than one server quotation marks should be used\n\t\t";
+  std::cout << "Format: --server serverName OR --server \"serverName1 serverName2 ... serverNameN\"";
+  std::cout << "\n\t\tBy default server is set to pool.ntp.org";
+  std::cout << "\n\t--time\n\t\t";
   std::cout << "set data and time for the updating data and time on the machine\n\t\tFormat:\n\t\t\t";
   std::cout << "Month/Day hour:minute, where:\n\t\t\t\t";
   std::cout << "Month: between 1 and 12 or 'every' for every month or 'every{interval}',\n\t\t\t\t\t";
@@ -13,7 +16,8 @@ void OutputHandler::printHelp() {
   std::cout << "hour: between 0 and 23 or 'every' for every hour or 'every{interval}',\n\t\t\t\t\t";
   std::cout << "where interval is between 1 and 23\n\t\t\t\t";
   std::cout << "minute: between 0 and 59 or 'every' for every minute or 'every{interval}',\n\t\t\t\t\t";
-  std::cout << "where interval is between 1 and 59\n\t\t\t\t\n";
+  std::cout << "where interval is between 1 and 59\n";
+  std::cout << "\t\tBy default command --time every/every 0:0 is called\n";
 }
 
 void OutputHandler::printMistakes(const std::vector<std::string>& mistakes) {
